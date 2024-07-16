75fa4767bab7e6c179f489ca2134da8a440c8ce6
Vishnu Nanilal Panicker <vishnunlal@gmail.com>
2024-07-16 19:27:16 +0530

    Added game projects' demo and code links.

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
 import { useState } from 'react';
 import './App.css';
-import {Link} from 'react-router-dom';
+import { Link } from 'react-router-dom';
 import Profile from './components/Profile';
 import ProjectCard from './components/ProjectCard';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faCaretUp, faCaretDown} from '@fortawesome/free-solid-svg-icons';
-import { faGithub} from '@fortawesome/free-brands-svg-icons';
+import { faCaretUp, faCaretDown } from '@fortawesome/free-solid-svg-icons';
+import { faGithub } from '@fortawesome/free-brands-svg-icons';
 
 function App() {
   const [showWebProjects, setShowWebProjects] = useState(false)
@@ -18,9 +18,15 @@ function App() {
   return (
     <div className="App">
       <header>
-      <Link to={"https://github.com/VishnuNanilal?tab=repositories"} style={{color: "var(--light2)"}}><FontAwesomeIcon icon={faGithub} /></Link>
-      <Link to={"https://github.com/VishnuNanilal?tab=repositories"} style={{color: "var(--light2)", textDecoration:"none"}}>LinkedIn</Link>
-      <Link to={"https://leetcode.com/u/vishnunlal/"} style={{color: "var(--light2)", textDecoration:"none"}}>LeetCode</Link>
+        <div className='nav-link'>
+          <Link to={"https://github.com/VishnuNanilal?tab=repositories"} style={{ color: "var(--light2)" }}><FontAwesomeIcon icon={faGithub} /></Link>
+        </div>
+        <div className='nav-link'>
+          <Link to={"https://www.linkedin.com/in/vishnu-n-p-871085151/  "} style={{ color: "var(--light2)", textDecoration: "none" }}>LinkedIn</Link>
+        </div>
+        <div className='nav-link'>
+          <Link to={"https://leetcode.com/u/vishnunlal/"} style={{ color: "var(--light2)", textDecoration: "none" }}>LeetCode</Link>
+        </div>
       </header>
       <main>
         <div className='main-center'>
@@ -41,12 +47,12 @@ function App() {
                 showGameProjects
                 &&
                 <div className='project-items-cont'>
-                  <ProjectCard name='Snake' demoLink='' codeLink='https://github.com/VishnuNanilal/Game-Snake'/>
-                  <ProjectCard name='Card Compat' demoLink='' codeLink='https://github.com/VishnuNanilal/Game-CardCompat'/>
-                  <ProjectCard name='Text Based Adventure' demoLink='' codeLink='https://github.com/VishnuNanilal/Game-TextBasedAdventure'/>
-                  <ProjectCard name='Chess' demoLink='' codeLink='https://github.com/VishnuNanilal/Game-Chess'/>
-                  <ProjectCard name='Area Calculation' demoLink='' codeLink=''/>
-                  <ProjectCard name='Factory Sorting' demoLink='' codeLink=''/>
+                  <ProjectCard name='Snake' demoLink='https://lucky-puffpuff-46e2bf.netlify.app/' codeLink='https://github.com/VishnuNanilal/Game-Snake' />
+                  <ProjectCard name='Card Compat' demoLink='https://resonant-crepe-c42fb0.netlify.app/' codeLink='https://github.com/VishnuNanilal/Game-CardCompat' />
+                  <ProjectCard name='Text Based Adventure' demoLink='https://glowing-cocada-826e7f.netlify.app/' codeLink='https://github.com/VishnuNanilal/Game-TextBasedAdventure' />
+                  <ProjectCard name='Chess' demoLink='https://669671d247c83b94e660bcef--idyllic-kitsune-998576.netlify.app/' codeLink='https://github.com/VishnuNanilal/Game-Chess' />
+                  <ProjectCard name='Area Calculation' demoLink='https://66966d03d13a7ca190672b46--cheerful-monstera-77e70c.netlify.app/' codeLink='https://github.com/VishnuNanilal/Area-Calculator' />
+                  <ProjectCard name='Factory Sorting' demoLink='https://66967b7cd560b1a13f166c0b--lustrous-pithivier-2b9b4a.netlify.app/' codeLink='https://github.com/VishnuNanilal/Factory-settings' />
                 </div>
               }
             </div>
